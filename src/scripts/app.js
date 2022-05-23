a919c4a7cda544d9bd37cdaf314d100edf474e19
migeotpierre <94454214+migeotpierre@users.noreply.github.com>
2022-05-23 17:30:00 +0200

    animation gsap introduction
    - header
    - Intro
    - Boutons
    - Mockup

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,30 @@
 import { gsap } from "gsap";
 
+gsap.from('.introduction__texte',{
+    x: -100,
+    duration: 1,
+    opacity: 0,
+
+})
+gsap.from('.image--introduction',{
+    x: 70,
+    duration: 1,
+    opacity: 0,
+    delay: 0.25,
+
+})
+gsap.from('.button-download',{
+    duration: 0.5,
+    opacity: 0,
+    delay: 1.5,
+
+})
+gsap.from('.header__logo',{
+    duration: 1.5,
+    opacity: 0,
+    delay: 2,
+
+})
+
+
 
