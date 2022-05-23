680eaaa6857880caeca99c3ca1e9084f28d19713
migeotpierre <94454214+migeotpierre@users.noreply.github.com>
2022-05-23 20:43:51 +0200

    gsap main + footer
    - description
    - images mockup iphone

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,9 +1,13 @@
 import { gsap } from "gsap";
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 gsap.from('.introduction__texte',{
     x: -100,
     duration: 1,
     opacity: 0,
+
 
 })
 gsap.from('.image--introduction',{
@@ -22,9 +26,99 @@ gsap.from('.button-download',{
 gsap.from('.header__logo',{
     duration: 1.5,
     opacity: 0,
-    delay: 2,
+    delay: 0.3,
 
 })
+
+gsap.from(".image1",{
+
+    x: -100,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".image1",
+        start: "center 90%",
+        end:"center 50%",
+        scrub: true,
+    },
+})
+gsap.from(".image2",{
+    x: 100,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".image2",
+        start: "center 90%",
+        end:"center 50%",
+        scrub: true,
+    },
+})
+gsap.from(".image3",{
+    x: -100,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".image3",
+        start: "center 90%",
+        end:"center 50%",
+        scrub: 0.5,
+    },
+})
+gsap.from(".explication1",{
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".explication1",
+        start: "center 90%",
+        end:"center 50%",
+        scrub: 0.5,
+    },
+})
+gsap.from(".explication2",{
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".explication2",
+        start: "center 90%",
+        end:"center 50%",
+        scrub: 0.5,
+    },
+})
+gsap.from(".explication3",{
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".explication3",
+        start: "center 90%",
+        end:"center 50%",
+        scrub: 0.5,
+    },
+})
+gsap.from(".image4",{
+    scale: 1.1,
+    duration: 3,
+    scrollTrigger: {
+        trigger: ".image4",
+        start: "center 90%",
+        end:"center 10%",
+        scrub: 0.5,
+    },
+})
+gsap.from(".description__texte",{
+    scale: 1.1,
+    duration: 3,
+    scrollTrigger: {
+        trigger: ".description__texte",
+        start: "center 90%",
+        end:"center 10%",
+        scrub: 0.5,
+    },
+})
+
+
+
+
+
 
 
 
